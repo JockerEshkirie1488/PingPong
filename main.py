5a051f6bbc839653a575b8350685857305f6ff79
JockerEshkirie1488 <166137098+JockerEshkirie1488@users.noreply.github.com>
2024-04-06 14:00:47 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ bg = transform.scale(
 
 class GameSprite(sprite.Sprite):
 
-    def __init__(self, x, y, w, h, s, im, rockettype):
+    def __init__(self, x, y, w, h, s, im):
         self.image = transform.scale(
             image.load(im),
             (w, h)
@@ -19,7 +19,6 @@ class GameSprite(sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         self.s = s
-        self.rockettype = rockettype
     
     def reset(self):
         win.blit(self.image, (self.rect.x, self.rect.y))
@@ -27,7 +26,8 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
 
     def __init__(self, x, y, w, h, s, im, rockettype):
-        super().__init__(x, y, w, h, s, im, rockettype)
+        super().__init__(x, y, w, h, s, im)
+        self.rockettype = rockettype
     
     def move(self):
         keys_pressed = key.get_pressed()
@@ -42,8 +42,26 @@ class Player(GameSprite):
             if keys_pressed[K_DOWN] and self.rect.y < 680:
                 self.rect.y += self.s
 
-r1 = Player(160, 360, 25, 160, 5, "rocket.png", 1)
-r2 = Player(1120, 360, 25, 160, 5, "rocket.png", 2)
+class Enemy(GameSprite):
+
+    def __init__(self, x, y, w, h, s, im, speedx, speedy):
+        super().__init__(x, y, w, h, s, im)
+        self.speedx = speedx
+        self.speedy = speedy
+    
+    def move(self):
+
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+        if self.rect.centery <= 60 or self.rect.centery >= 660:
+            self.speedy *= -1
+        if self.rect.centerx <= 60 or self.rect.centerx >= 1220:
+            self.speedx *= -1
+
+r1 = Player(160, 300, 25, 160, 5, "rocket.png", 1)
+r2 = Player(1120, 300, 25, 160, 5, "rocket.png", 2)
+ball = Enemy(240, 300, 128, 128, 6, "ball.png", 6, 5)
 
 clock = time.Clock()
 
@@ -57,8 +75,15 @@ while game:
         
     r1.move()
     r2.move()
+    ball.move()
+
+    if Rect.colliderect(ball.rect, r1.rect):
+        ball.speedx *= -1
+    if Rect.colliderect(ball.rect, r2.rect):
+        ball.speedx *= -1
     
     win.blit(bg, (0, 0))
+    ball.reset()
     r1.reset()
     r2.reset()
     
